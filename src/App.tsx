ae58f352b369892a22e8b82868e2a62baaa66b38
neanderdev <neander.faria@hotmail.com>
2022-02-19 17:08:23 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from './components/Header/index';
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
+  );
+}
