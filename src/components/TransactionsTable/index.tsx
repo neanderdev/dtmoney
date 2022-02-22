519c5947dc197e07e0efa88f6ad3073806c852ba
neanderdev <neander.faria@hotmail.com>
2022-02-22 08:31:40 -0300

    Formatting values ​​and date in Brazilian format

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -35,9 +35,12 @@ export function TransactionsTable() {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>R$ {transaction.amount}</td>
+              <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(transaction.amount)}</td>
               <td>{transaction.category}</td>
-              <td>{transaction.cratedAt}</td>
+              <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.cratedAt))}</td>
             </tr>
           ))}
         </tbody>
